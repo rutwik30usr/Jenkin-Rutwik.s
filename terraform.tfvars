177e1b712812a8f29110130078b206398e1482f6
rutwik <rutwiksatpute3034@gmail.com>
2025-11-12 01:25:19 +0530

    Remove duplicate region variable and finalize ap-south-1 infrastructure configuration

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,1 +1,7 @@
-region = "ap-south-1"
+
+region               = "ap-south-1"
+vpc_cidr             = "10.0.0.0/16"
+vpc_name             = "dev-proj-jenkins-ap-south-vpc-1"
+cidr_public_subnet   = ["10.0.1.0/24", "10.0.2.0/24"]
+cidr_private_subnet  = ["10.0.3.0/24", "10.0.4.0/24"]
+ap_availability_zone = ["ap-south-1a", "ap-south-1b"]
--- variable.tf
+++ variable.tf
@@ -3,3 +3,30 @@ variable "region" {
   type        = string
   default     = "ap-south-1"
 }
+
+
+
+variable "vpc_cidr" {
+  type        = string
+  description = "Public Subnet CIDR values"
+}
+
+variable "vpc_name" {
+  type        = string
+  description = "DevOps Project 1 VPC 1"
+}
+
+variable "cidr_public_subnet" {
+  type        = list(string)
+  description = "Public Subnet CIDR values"
+}
+
+variable "cidr_private_subnet" {
+  type        = list(string)
+  description = "Private Subnet CIDR values"
+}
+
+variable "ap_availability_zone" {
+  type        = list(string)
+  description = "Availability Zones"
+}
